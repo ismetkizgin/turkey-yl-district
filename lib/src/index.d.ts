28a0af266db8a63a2bac04327f97fcf3bdc0e9d2
ismet kizgin <info@ismetkizgin.com>
2020-09-19 22:45:30 +0300

    ci: project build for npm upload

---
+++ lib/src/index.d.ts
@@ -0,0 +1,5 @@
+export declare function getCityNameAndPlates(): object;
+export declare function getDistrictsName(city: any): object;
+export declare function getCityName(): object;
+export declare function getNeighbourhoods(cityName: string, districtName: string): object;
+//# sourceMappingURL=index.d.ts.map
