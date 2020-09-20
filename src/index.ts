704cbd6f373e1bd1da5a40241c0c40f6bb6bc2cd
ismet kizgin <info@ismetkizgin.com>
2020-09-20 13:05:59 +0300

    perf:  promise structure removed

--- src/index.ts
+++ src/index.ts
@@ -1,53 +1,24 @@
 import { CityNames, CityPlates, Districts, Neighbourhoods } from "./data";
 
 export function getCityNameAndPlates(): any {
-  return new Promise((resolve, reject) => {
-    try {
-      resolve(CityPlates);
-    } catch (error) {
-      reject(error);
-    }
-  });
+  return CityPlates;
 }
 
 export function getDistrictsName(city: any): object {
-  return new Promise((resolve, reject) => {
-    try {
-      resolve(
-        (<any>Districts).find((district: any) => {
-          if (district.CityName === city || district.PlatesNo === 21)
-            return district;
-        })
-      );
-    } catch (error) {
-      reject(error);
-    }
+  return (<any>Districts).find((district: any) => {
+    if (district.CityName === city || district.PlatesNo === 21) return district;
   });
 }
 
 export function getCityName(): object {
-  return new Promise((resolve, reject) => {
-    try {
-      resolve(CityNames);
-    } catch (error) {
-      reject(error);
-    }
-  });
+  return CityNames;
 }
 
 export function getNeighbourhoods(
   cityName: string,
   districtName: string
 ): object {
-  return new Promise((resolve, reject) => {
-    try {
-      resolve(
-        (<any>Neighbourhoods)[cityName.toLocaleUpperCase("tr-TR")][
-          districtName.toLocaleUpperCase("tr-TR")
-        ]
-      );
-    } catch (error) {
-      reject(error);
-    }
-  });
+  return (<any>Neighbourhoods)[cityName.toLocaleUpperCase("tr-TR")][
+    districtName.toLocaleUpperCase("tr-TR")
+  ];
 }
